fe36918aedccf138d0a96ca44a7e8844120faf69
Noronha1612 <inc.691@gmail.com>
2020-09-30 22:23:27 -0300

    Base structure created

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import celebrate from 'celebrate';
+
+const routes = Router();
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+
+import routes from './routes';
+
+const port = 3333;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use(errors());
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
